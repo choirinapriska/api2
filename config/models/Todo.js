61753830a7e88f84a51de8702d45b532a1d1c226
choirinapriska <priskachoirina17@gmail.com>
2017-12-06 12:33:08 +0700

    ss1

--- config/models/Todo.js
+++ config/models/Todo.js
@@ -14,14 +14,6 @@ module.exports = {
 	    	password : process.env.MYSQL_ADDON_PASSWORD
 	    });
 
-	    connection.connect(function(err) {
-		  if (err) {
-		    console.error('error connecting: ' + err.stack);
-		    return;
-		  }
-
-		  console.log('connected as id ' + connection.threadId);
-		});
 
 	    return connection;
     },
